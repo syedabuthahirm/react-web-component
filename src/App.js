42b18572f2d28e9567c1d9765eaf95cb547f95c5
syedabuthahirm <abuthahir741@gmail.com>
2020-03-09 21:30:20 +0530

    Test WC in React

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <my-component first="hello" last="there" />
+      <syd-login-form></syd-login-form>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { defineCustomElements } from "syd-form/loader";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+defineCustomElements(window);

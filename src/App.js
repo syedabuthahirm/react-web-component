d14e855ff42c5e8fe8baa957360a0c5cc1c5c9fd
syedabuthahirm <abuthahir741@gmail.com>
2020-03-12 00:44:53 +0530

    Added Login Form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
 function App() {
+  const formEl = useRef(null);
+
+  useEffect(() => {
+    if (formEl) {
+      formEl.current.addEventListener("formSubmit", handleNvEnter);
+    }
+  }, []);
+
+  const handleNvEnter = e => {
+    console.log("clicked", e);
+  };
+
   return (
     <div className="App">
       <my-component first="hello" last="there" />
-      <syd-login-form></syd-login-form>
+      <syd-login-form ref={formEl}></syd-login-form>
     </div>
   );
 }
